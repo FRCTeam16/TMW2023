847917209440da476d736039f21759d5748efd5e
FRC Team 16 <frcteam16@gmail.com>
2023-02-19 00:34:12 -0600

    testing out some example auto

--- src/main/java/frc/robot/auto/AutoManager.java
+++ src/main/java/frc/robot/auto/AutoManager.java
@@ -6,12 +6,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Subsystems;
 import frc.robot.auto.strategies.DebugAuto;
+import frc.robot.autos.exampleAuto;
 
 public class AutoManager {
 
     public enum AutoStrategies {
-        DebugAuto
+        DebugAuto, ExampleAuto
     }
 
     private final SendableChooser<AutoStrategies> chooser = new SendableChooser<>();
@@ -19,6 +21,7 @@ public class AutoManager {
 
     public AutoManager() {
         registerStrategy("Debug Auto", AutoStrategies.DebugAuto, new DebugAuto());
+        registerStrategy("exampleAuto", AutoStrategies.ExampleAuto, new exampleAuto(Subsystems.swerveSubsystem));
 
         // Send selector Dashboard.  If it doesn't show in SD, you may need to change the name here.
         SmartDashboard.putData("Auto Selector", chooser);
