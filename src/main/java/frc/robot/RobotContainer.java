03bdd2148933c6f53d967aaea1e3d7f095a83cd9
FRC Team 16 <frcteam16@gmail.com>
2023-03-17 22:09:04 -0500

    Emma is a button masher

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -53,8 +53,8 @@ public class RobotContainer {
     private final JoystickButton intake    = new JoystickButton(right,    1);
     private final JoystickButton eject     = new JoystickButton(left,   1);
     private final JoystickButton lockAngle180 = new JoystickButton(left, 2);
-    private final Trigger lockAngleN90 = new Trigger(() -> left.getPOV() == 0);
-    private final Trigger lockAngleN0 = new Trigger(() -> right.getPOV() == 0);
+    private final Trigger lockAngleN90 = new Trigger(() -> left.getPOV() >= 0);
+    private final Trigger lockAngleN0 = new Trigger(() -> right.getPOV() >= 0);
 //    private final JoystickButton lockAngleN90 = new JoystickButton(left, 7);    // FIXME
       
     private final Trigger wristOpenLoopDown = new JoystickButton(gamepad, XboxController.Button.kLeftBumper.value);
