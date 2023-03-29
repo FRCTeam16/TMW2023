217315d0e132370b3912e51786a498e149d6cfa9
FRC Team 16 <frcteam16@gmail.com>
2023-03-28 20:46:31 -0500

    don't go to intermediate if moving from mid score to high

--- src/main/java/frc/robot/commands/pose/MoveToScoreConeHighPose.java
+++ src/main/java/frc/robot/commands/pose/MoveToScoreConeHighPose.java
@@ -4,43 +4,32 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Subsystems;
+import frc.robot.commands.pose.PoseManager.Pose;
 import frc.robot.subsystems.Elevator.ElevatorPosition;
 import frc.robot.subsystems.Intake.WristPosition;
 import frc.robot.subsystems.Pivot.PivotPosition;
 
 class MoveToScoreConeHighPose extends SequentialCommandGroup {
     public MoveToScoreConeHighPose() {
-        if(Subsystems.pivot.getPivotEncoderPosition() > 30000) {
+        if(Subsystems.pivot.getPivotEncoderPosition() > 30000 &&
+           Subsystems.poseManager.getLastPose() != Pose.ScoreMidCone) {
             addCommands(
                 new PosePivot(PivotPosition.Intermediate),
                 new PoseElevator(ElevatorPosition.Intermediate),
-                new WaitCommand(.25),
-                Commands.parallel(
-                    new PosePivot(PivotPosition.ScoringAngle),
-                    new PoseElevator(ElevatorPosition.ScoreConeHigh)
-                ),
-                new WaitCommand(0.25),
-                Commands.parallel(
-                    Subsystems.intake.isHandOpen() ? 
-                        new PoseWrist(WristPosition.ScoreCubeHigh) :
-                        new PoseWrist(WristPosition.ScoreCone)
-                )
+                new WaitCommand(.25)
             );
         }
-
-        else {
-            addCommands(
-                Commands.parallel(
-                    new PosePivot(PivotPosition.ScoringAngle),
-                    new PoseElevator(ElevatorPosition.ScoreConeHigh)
-                ),
-                new WaitCommand(0.25),
-                Commands.parallel(
-                    Subsystems.intake.isHandOpen() ? 
-                        new PoseWrist(WristPosition.ScoreCubeHigh) :
-                        new PoseWrist(WristPosition.ScoreCone)
-                )
-            );
-        }
+        addCommands(
+            Commands.parallel(
+                new PosePivot(PivotPosition.ScoringAngle),
+                new PoseElevator(ElevatorPosition.ScoreConeHigh)
+            ),
+            new WaitCommand(0.25),
+            Commands.parallel(
+                Subsystems.intake.isHandOpen() ? 
+                    new PoseWrist(WristPosition.ScoreCubeHigh) :
+                    new PoseWrist(WristPosition.ScoreCone)
+            )
+        );
     }
 }
